4fac2d7bbdfeb561803337dd954fc202547f5a49
John Fiewor <johnfiewor@gmail.com>
2021-09-13 16:59:45 +0100

    add styling

--- src/App.js
+++ src/App.js
@@ -20,21 +20,38 @@ const list = [
     objectID: 1
   }
 ]
+// inline styles
+const largeColumn = {
+    width: '40%'
+}
+
+const midColumn = {
+    width: '30%'
+}
+
+const smallColumn = {
+    width: '10%'
+}
 
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
 
 const Table = ({list, pattern, onDismiss}) =>
-    <div>
+    <div className="table">
         {list.filter(isSearched(pattern)).map(item =>    
-        <div key={item.objectID}>
-            <span>
+        <div key={item.objectID} className="table-row">
+            <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-            <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+            <span style={midColumn}>{item.author}</span>
+            <span style={smallColumn}>{item.num_comments}</span>
+            <span style={smallColumn}>{item.points}</span>
+            <span style={smallColumn}>
+            <Button
+                onClick={() => onDismiss(item.objectID)}
+                className="button-inline"
+            >
+            Dismiss
+            </Button>
             </span>
         </div>)}
     </div>
@@ -78,13 +95,15 @@ class App extends Component {
   render() {
     const {list, searchTerm} = this.state
     return(
-      <div className="App">
-        <Search 
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search:
-        </Search>
+      <div className="page">
+        <div className="interactions">
+            <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            >
+            Search:
+            </Search>
+        </div>
         <Table
             list={list}
             pattern={searchTerm}
