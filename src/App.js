6773efd2dd917d7f17cf7345766514bf4b31814a
John Fiewor <johnfiewor@gmail.com>
2021-10-13 12:59:41 +0100

    lift state. add classname pkg. update test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
 import { sortBy } from 'lodash'
+import classNames from 'classnames'
 import './App.css'
 
 const DEFAULT_QUERY = 'react'
-const DEFAULT_HPP = '100'
+const DEFAULT_HPP = '5'
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search'
@@ -34,33 +35,56 @@ const SORTS = {
   POINTS: list => sortBy(list, 'points').reverse(),
 }
 
-const Table = ({list, sortKey, isSortReverse, onSort, onDismiss}) => 
-  {
+class Table extends Component {
+  constructor(props) { 
+    super(props) 
+    
+    this.state = {
+      sortKey: 'NONE',
+      isSortReverse: false,
+    }
+    this.onSort = this.onSort.bind(this)
+  }
+
+  onSort(sortKey) {
+    const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse
+    this.setState({ sortKey, isSortReverse })
+  }
+  
+  render() {
+    const {
+      list,
+      sortKey,
+      isSortReverse,
+      onSort,
+      onDismiss
+    } = this.props
+
     const sortedList = SORTS[sortKey](list)
-    const reverseSortedeList = isSortReverse
+    const reverseSortedList = isSortReverse
       ? sortedList.reverse()
       : sortedList
-
+  
       return (
         <div className="table">
           <div className="table-header">
             <span stlye={{ width: '40%' }}>
-              <Sort sortKey={'TITLE'} onSort={onSort}> Title </Sort>
+              <Sort sortKey={'TITLE'} onSort={onSort} activeSortKey={sortKey}> Title </Sort>
             </span>
             <span stlye={{ width: '30%' }}>
-              <Sort sortKey={'AUTHOR'} onSort={onSort}> Author </Sort>
+              <Sort sortKey={'AUTHOR'} onSort={onSort} activeSortKey={sortKey}> Author </Sort>
             </span>
             <span style={{ width: '10%' }}>
-              <Sort sortKey={'COMMENTS'} onSort={onSort}> Comments </Sort>
+              <Sort sortKey={'COMMENTS'} onSort={onSort} activeSortKey={sortKey}> Comments </Sort>
             </span>
             <span style={{ width: '10%' }}>
-              <Sort sortKey={'POINTS'} onSort={onSort}> Points </Sort>
+              <Sort sortKey={'POINTS'} onSort={onSort} activeSortKey={sortKey}> Points </Sort>
             </span>
             <span style={{ width: '10%' }}> Archive </span>
           </div>
-
+  
           {/* {reverseSortedList.map(item => ... )} */}
-
+  
             {SORTS[sortKey](list).map(item =>    
               <div key={item.objectID} className="table-row">
                   <span style={largeColumn}>
@@ -71,7 +95,7 @@ const Table = ({list, sortKey, isSortReverse, onSort, onDismiss}) =>
                   <span style={smallColumn}>{item.points}</span>
                   <span style={smallColumn}>
                   <Button
-                    onClick={() => onDismiss(item.objectID)}
+                    onClick={() => this.onDismiss(item.objectID)}
                     className="button-inline"
                   >
                   Dismiss
@@ -79,12 +103,12 @@ const Table = ({list, sortKey, isSortReverse, onSort, onDismiss}) =>
                   </span>
               </div>
             )}
-
+  
         </div>
-
+  
       )
   }
-
+}
 
 Table.propTypes = {
   list: PropTypes.arrayOf(
@@ -128,7 +152,7 @@ const Loading = () => <div>Loading...</div>
 // higher order component. takes a component as input(and maybe some arguments) and returns a component (enhanced version of the input) as output
 // const withLoading = (Component) => (props) => // since the input component may not care about the isLoading property, use the rest destructuring to avoid that(?) instead of just spreading/passing all the eprops
 const withLoading = (Component) => ({ isLoading, ...rest }) =>
-isLoading // based on the loading property, apply a conditional rendering. this function will return the Loading gcomponent or the functional component
+isLoading // based on the loading property, apply a conditional rendering. this function will return the Loading component or the functional component
 ? <Loading />
 : <Component { ...rest } />
 // so this takes one property out of the object and keeps the remaining object
@@ -137,10 +161,27 @@ isLoading // based on the loading property, apply a conditional rendering. this 
 const ButtonWithLoading = withLoading(Button) // this is the enhanced output component
 
 // Sort component which upon click calls the onCLIck function that uses the onSort() method to sort the list based on selected column header
-const Sort = ({ sortKey, onSort, children }) =>
-  <Button onClick={() => onSort(sortKey)} className="button-inline">
+const Sort = ({ sortKey, activeSortKey, onSort, children }) => {
+  // clumsy way:
+  // const sortClass = ['button-inline']
+  // if(sortKey === activeSortKey) {
+  //   sortClass.push('button-active')
+  // }
+
+  // using classnames package:
+  const sortClass = classNames(
+    'button-inline',
+    {  'button-active': sortKey === activeSortKey }
+  )
+
+  return (
+  <Button
+    onClick={() => onSort(sortKey)}
+    className={sortClass}>
     {children}
   </Button>
+  )
+}
 
 class App extends Component {
   _isMounted = false
@@ -153,8 +194,6 @@ class App extends Component {
       searchTerm: DEFAULT_QUERY,
       error: null,
       isLoading: false,
-      sortKey: 'NONE',
-      isSortReverse: false
     }
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
@@ -162,12 +201,6 @@ class App extends Component {
     this.onSearchChange = this.onSearchChange.bind(this)
     this.onSearchSubmit = this.onSearchSubmit.bind(this)
     this.onDismiss = this.onDismiss.bind(this)
-    this.onSort = this.onSort.bind(this)
-  }
-
-  onSort(sortKey) {
-    const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse
-    this.setState({ sortKey, isSortReverse })
   }
 
   needsToSearchTopStories(searchTerm){
@@ -177,6 +210,7 @@ class App extends Component {
   setSearchTopStories(result) {
     const {hits, page} = result
     const {searchKey, results} = this.state
+
     const oldHits = results && results[searchKey]
     ? results[searchKey].hits
     : []
@@ -189,7 +223,7 @@ class App extends Component {
     this.setState({
       results: {
         ...results,
-        [searchKey]:{ hits: updatedHits, page }
+        [searchKey]: { hits: updatedHits, page }
       },
       isLoading: false
     })
@@ -241,13 +275,19 @@ class App extends Component {
     this.setState({
       results: {
         ...results,
-        [searchKey]: {hits: updatedHits, page}
+        [searchKey]: { hits: updatedHits, page }
       }
     })
   }
 
   render() {
-    const {results, searchTerm, searchKey, error, isLoading, sortKey} = this.state
+    const {
+      searchTerm,
+      results,
+      searchKey,
+      error,
+      isLoading,
+    } = this.state
     const page = (results && results[searchKey] && results[searchKey].page) || 0
     const list = (results && results[searchKey] && results[searchKey].hits) || []
 
@@ -272,18 +312,15 @@ class App extends Component {
             </ButtonWithLoading>
           }
         </div>
-        {error
-        ? <div className="interactions">
-            <p>Uhm...Something went wrong</p>
-            <p>List was unable to load</p>
-          </div>
-        : <Table
-            list={list}
-            sortKey={sortKey}
-            isSortReverse={isSortReverse}
-            onSort={this.onSort}
-            onDismiss={this.onDismiss}
-          />
+        { error
+          ? <div className="interactions">
+              <p>Uhm...Something went wrong</p>
+              <p>List was unable to load</p>
+            </div>
+          : <Table
+              list={list}
+              onDismiss={this.onDismiss}
+            />
         }
         </div>
     )
